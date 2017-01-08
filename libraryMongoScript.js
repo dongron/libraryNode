197618b128db1e6dde14a99de2c53890c2fc35d6
Dominik <dongron@wp.pl>
2017-01-08 16:46:24 +0100

    Zmnieniono skrypt dodajacy dane do bazy MongoDB.

--- libraryMongoScript.js
+++ libraryMongoScript.js
@@ -1,54 +1,11 @@
-//use library;
+// data
+use library;
 
-//db.createUser(
-//   {
-//     user: "test",
-//     pwd: "test",
-//     roles:
-//       [
-//         { role: "readWrite", db: "library" }
-//       ]
-//   }
-//)
+db.books.insert({name:"Kot w butach", author: "Grimm", year: "1960", review: "Great book!", rentBy: ""});
+db.books.insert({name:"Mikolajek", author: "Goscinny", year: "1986", review: "", rentBy: ""});
+db.books.insert({name:"Harry Potter 1", author: "J.K. Rowling", year: "1999", review: "", rentBy: ""});
 
-//db.books.insert({name:"Kot w butach", author: "Grimm", year: "1960", review: "", rentBy: ""});
-//db.books.insert({name:"Kot w butach", author: "Grimm", year: "1960", review: "", rentBy: ""});
-//db.books.insert({name:"Mikolajek", author: "Goscinny", year: "1986", review: "", rentBy: ""});
-//db.books.insert({name:"Harry Potter 1", author: "Rowling", year: "1999", review: "", rentBy: ""});
-//db.users.insert({login: "test", password: "test"});
-//db.users.insert({login: "test1", password: "test1"});
-//db.users.insert({login: "test2", password: "test2"});
+db.users.insert({login: "test", password: "test"});
+db.users.insert({login: "test1", password: "test1"});
+db.users.insert({login: "test2", password: "test2"});
 
-
-
-
-
-// inne: 
-//db.books.remove({'name':'Kot w butach'});
-//db.books.remove({'rentBy':'58666affbb569757a028012f'});
-//db.books.insert({name:"Kot w butach", author: "Grimm", year: "1960", review: "", rentBy: ""});
-//show collections;
-//db.rozne.drop();
-
-//db.dupa.drop();
-
-
-//db.books.update({"name": "Mikolajek"}, { $set: {"rentBy": "58666affbb569757a028012f"}});
-
-
-
-
-
-
-
-
-
-// nie użyte: 
-
-//db.books.update({"name": "Mikolajek"}, {"rentBy": "58666affbb569757a028012f"});
-//db.createCollection("books", {autoIndexID : true} ) {  }
-//db.createCollection("user", autoIndexID : true) { 
-//  "login" : 1 ,
-//  "password" : 1 ,
-//  };
-  
